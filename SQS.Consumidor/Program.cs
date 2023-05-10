1937f24b85fa28dce55713624e10444bdb57d24c
Saulo <saulomas@gmail.com>
2023-05-10 10:41:28 -0300

    Add project files.

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,25 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+const string urlQueue = "https://sqs.sa-east-1.amazonaws.com/480910489369/teste";
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new ReceiveMessageRequest()
+{
+    QueueUrl = urlQueue
+};
+
+while (true)
+{
+    var response = await client.ReceiveMessageAsync(request);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine(message.Body);
+        await client.DeleteMessageAsync(urlQueue, message.ReceiptHandle);
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,13 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+const string urlQueue = "https://sqs.sa-east-1.amazonaws.com/480910489369/teste";
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new SendMessageRequest(urlQueue, "Teste 123.");
+
+await client.SendMessageAsync(request);
